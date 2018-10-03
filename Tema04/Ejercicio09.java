add69896ab886de605bfacecfb3f6b55992af85a
Iván Pérez Molina <ivan.perez.8500@gmail.com>
2018-10-03 08:19:18 +0200

    Subiendo nuevo contenido

---
+++ Tema04/Ejercicio09.java
@@ -0,0 +1,46 @@
+/**
+* Ejercicio 9
+*
+* @author Iván Pérez Molina
+*/
+
+public class Ejercicio09 {
+  public static void main(String[] args) {
+
+    String linea;
+    System.out.print("Introduce el valor de a : ");
+    linea = System.console().readLine();
+    double a;
+    a = Double.parseDouble( linea );
+    
+    String lineados;
+    System.out.print("Introduce el valor de b:");
+    lineados = System.console().readLine();
+    double b;
+    b = Double.parseDouble( lineados );
+    
+    String lineatres;
+    System.out.print("Introduce el valor de c:");
+    lineatres = System.console().readLine();
+    double c;
+    c = Double.parseDouble( lineatres );
+
+    if (a == 0.0){
+      System.out.print("ERROR FATAL!");
+    }
+    
+    else {
+    double total1;
+    total1 = (-b + Math.sqrt ((b*b)-(4*a*c))) / 2*a ;
+    
+    double total2;
+    total2 = (-b - Math.sqrt ((b*b)-(4*a*c))) / 2*a ;
+
+    System.out.println("El primer número introducido es " + a);
+    System.out.println("El segundo numero introducido es  " + b);
+    System.out.println("El tercer numero introducido es  " + c);
+    System.out.println("El resultado de su operacion al hacer la formula en positivo es  :" + total1);
+    System.out.println("El resultado de su operacion al hacer la formula en negativo es  :" + total2);
+    }
+  } 
+}
