1a74ddee7718d8b37ca06e253c06b66a094ebbd3
Iván Pérez Molina <ivan.perez.8500@gmail.com>
2018-10-04 08:38:37 +0200

    Actualizando el jueves

---
+++ Tema04/Ejercicio13.java
@@ -0,0 +1,89 @@
+/**
+* Ejercicio que me ordena los numeros de menor a mayor
+*
+* @author Iván Pérez Molina
+*/
+
+public class Ejercicio13 {
+  public static void main(String[] args) {
+
+    //Pedir los valores de los tres numeros
+    System.out.println("BINVENIDO AL PROGRAMA PARA ORDENAR SUS TRES NUMEROS DE MENOR A MAYOR");
+    System.out.println("PORFAVOR, NO REPITAS LOS NUMEROS O TU PC EXPLOTARA");
+    String linea;
+    System.out.println("Introduce el primer numero: ");
+    linea = System.console().readLine();
+    int a;
+    a = Integer.parseInt( linea );
+    
+    String lineados;
+    System.out.println("Introduce el segundo numero: ");
+    lineados = System.console().readLine();
+    int b;
+    b = Integer.parseInt( lineados );
+        
+    String lineatres;
+    System.out.println("Introduce el tercer numero: ");
+    lineatres = System.console().readLine();
+    int c;
+    c = Integer.parseInt( lineatres );
+    
+    //Vamos comparando los valores para hacer el orden
+      if ((a > b) && (a > c) && (b > c)){
+        System.out.println("He ordenado los numeros y:  ");
+        System.out.println("El primer valor es el: " + a );
+        System.out.println("El segundo valor es el: " + b);
+        System.out.println("El tercer valor es el: " + c );
+      
+      } else { if ((b > a) && (b > c) && (a > c)){
+               System.out.println("He ordenado los numeros y:  ");
+               System.out.println("El primer valor es el: " + b );
+               System.out.println("El segundo valor es el: " + a);
+               System.out.println("El tercer valor es el: " + c );
+             } else { if ((c > a) && (c > b) && (a > b)) {
+                          System.out.println("He ordenado los numeros y:  ");
+                          System.out.println("El primer valor es el: " + c );
+                          System.out.println("El segundo valor es el: " + a);
+                          System.out.println("El tercer valor es el: " + b );
+                      } else { if ((c > a) && (c > b) && (b > a)){
+                                  System.out.println("He ordenado los numeros y:  ");
+                                  System.out.println("El primer valor es el: " + c );
+                                  System.out.println("El segundo valor es el: " + b);
+                                  System.out.println("El tercer valor es el: " + a );
+                              } else { if ((a > c) && (a > b) && (c > b)) {
+                                        System.out.println("He ordenado los numeros y:  ");
+                                        System.out.println("El primer valor es el: " + a );
+                                        System.out.println("El segundo valor es el: " + c);
+                                        System.out.println("El tercer valor es el: " + b );
+                                      } else { if ((b > a) && (b > a) && (c > a)) {
+                                                System.out.println("He ordenado los numeros y:  ");
+                                                System.out.println("El primer valor es el: " + b );
+                                                System.out.println("El segundo valor es el: " + c);
+                                                System.out.println("El tercer valor es el: " + a );
+                                              }
+                                        
+                                        
+                                        
+                                        
+                                        }
+                                }
+                        }
+               }
+        }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Tema04/Ejercicio16.java
@@ -0,0 +1,226 @@
+/**
+* Infidelidad de nuestra pareja
+*
+* @author Iván Pérez Molina
+*/
+
+public class Ejercicio16 {
+  public static void main(String[] args) {
+
+    //Declaro una variable que utilizare más adelante para contar los aciertos.
+    int contador;
+    contador = 0 ;
+    
+    
+    //PREGUNTA 1
+    System.out.println("Bienvenido al test de infidelidad. Descubre si tu pareja te será infiel!!");
+    System.out.println("\n");
+    System.out.println("Contesta con V o F");
+    String linea;
+    System.out.println("1. Tu pareja parece estar más inquieta de lo normal sin ningún motivo aparente.");
+    linea = System.console().readLine();
+    int preguntaUno;
+    preguntaUno = Integer.parseInt( linea );
+    
+    if (preguntaUno == "V"){
+      System.out.println("HAS CONTESTADO VERDADERO");
+      contador = contador + 3;
+    } else {
+      System.out.println("HAS CONTESTADO FALSO ");
+
+      }
+      
+      
+    //PREGUNTA 2
+    System.out.println("\n");
+    System.out.println("\n");
+    String lineados;
+    System.out.println("2. Ha aumentado sus gastos de vestuario");
+    lineados = System.console().readLine();
+    int preguntaDos;
+    preguntaDos = Integer.parseInt( lineados );
+    
+    if (preguntaDos == "V"){
+      System.out.println("HAS CONTESTADO VERDADERO ");
+      contador = contador + 3;
+    } else {
+      System.out.println("HAS CONTESTADO FALSO ");
+      }
+      
+    //PREGUNTA 3
+    System.out.println("\n");
+    System.out.println("\n");
+    String lineatres;
+    System.out.println("3. Ha perdido el interés que mostraba anteriormente por ti");
+    lineatres = System.console().readLine();
+    int preguntaTres;
+    preguntaTres = Integer.parseInt( lineatres );
+    
+    if (preguntaTres == "V"){
+      System.out.println("HAS CONTESTADO VERDADERO ");
+      contador = contador + 3;
+    } else {
+      System.out.println("HAS CONTESTADO FALSO ");
+      }
+      
+    //PREGUNTA 4
+    System.out.println("\n");
+    System.out.println("\n");
+    String lineacuatro;
+    System.out.println("4. Ahora se afeita y se asea con más frecuencia (si es hombre) o ahora se arregla el pelo y se asea con más frecuencia (si es mujer)");
+    
+    lineacuatro = System.console().readLine();
+    int preguntaCuatro;
+    preguntaCuatro = Integer.parseInt( lineacuatro );
+    
+    if (preguntaCuatro == "V"){
+      System.out.println("HAS CONTESTADO VERDADERO");
+      contador = contador + 3;
+    } else {
+      System.out.println("HAS CONTESTADO FALSO ");
+      }
+      
+    //PREGUNTA 5
+    System.out.println("\n");
+    System.out.println("\n");
+    String lineacinco;
+    System.out.println("5. No te deja que mires la agenda de su teléfono móvil");
+    lineacinco = System.console().readLine();
+    int preguntaCinco;
+    preguntaCinco = Integer.parseInt( lineacinco );
+    
+    if (preguntaCinco == "V"){
+      System.out.println("HAS CONTESTADO VERDADERO");
+      contador = contador + 3;
+    } else {
+      System.out.println("HAS CONTESTADO FALSO");
+      }
+      
+    //PREGUNTA 6
+    System.out.println("\n");
+    System.out.println("\n");
+    String lineaseis;
+    System.out.println("6. A veces tiene llamadas que dice no querer contestar cuando estás tú delante");
+    lineaseis = System.console().readLine();
+    int preguntaSeis;
+    preguntaSeis = Integer.parseInt( lineaseis );
+    
+    if (preguntaSeis == "V"){
+      System.out.println("HAS CONTESTADO VERDADERO ");
+      contador = contador + 3;
+    } else {
+      System.out.println("HAS CONTESTADO FALSO");
+      }
+      
+    //PREGUNTA 7
+    System.out.println("\n");
+    System.out.println("\n");
+    String lineasiete;
+    System.out.println("7. Últimamente se preocupa más en cuidar la línea y/o estar bronceado/a");
+    lineasiete = System.console().readLine();
+    int preguntaSiete;
+    preguntaSiete = Integer.parseInt( lineasiete );
+    
+    if (preguntaSiete == "V"){
+      System.out.println("HAS CONTESTADO VERDADERO ");
+      contador = contador + 3;
+    } else {
+      System.out.println("HAS CONTESTADO FALSO");
+      contador = contador + 0;
+      }
+      
+    //PREGUNTA 8
+    System.out.println("\n");
+    System.out.println("\n");
+    String lineaocho;
+    System.out.println("8. Muchos días viene tarde después de trabajar porque dice tener mucho más trabajo");
+    lineaocho = System.console().readLine();
+    int preguntaOcho;
+    preguntaOcho = Integer.parseInt( lineaocho );
+    
+    if (preguntaOcho == "V"){
+      System.out.println("HAS CONTESTADO VERDADERO");
+      contador = contador + 3;
+    } else {
+      System.out.println("HAS CONTESTADO FALSO ");
+      }
+      
+    //PREGUNTA 9
+    System.out.println("\n");
+    System.out.println("\n");
+    String lineanueve;
+    System.out.println("9. Has notado que últimamente se perfuma más");
+    lineanueve = System.console().readLine();
+    int preguntaNueve;
+    preguntaNueve = Integer.parseInt( lineanueve );
+    
+    if (preguntaNueve == "V"){
+      System.out.println("HAS CONTESTADO VERDADERO ");
+      contador = contador + 3;
+    } else {
+      System.out.println("HAS CONTESTADO FALSO ");
+      contador = contador + 0;
+      }
+      
+    //PREGUNTA 10
+    System.out.println("\n");
+    System.out.println("\n");
+    String lineadiez;
+    System.out.println("10. Se confunde y te dice que ha estado en sitios donde no ha ido contigo");
+    lineadiez = System.console().readLine();
+    int preguntaDiez;
+    preguntaDiez = Integer.parseInt( lineadiez );
+    
+    if (preguntaDiez == "V"){
+      System.out.println("HAS CONTESTADO VERDADERO ");
+      contador = contador + 3;
+    } else {
+      System.out.println("HAS CONTESTADO FALSO");
+      contador = contador + 0;
+      }
+          System.out.println("\n");
+          System.out.println("\n");
+          System.out.println("El número de 'VERDADEROS' en el test es de " +contador);
+          
+          
+          if (contador <=30) && (contador >=22){
+            System.out.println("Tu pareja tiene todos los ingredientes para estar viviendo un romance con otra persona. Te aconsejamos que indagues un poco más y averigües que es lo que está pasando por su cabeza."); 
+          } 
+          else { 
+            if ((contador <22)&&(contador > 11)){
+                     System.out.println("Quizás exista el peligro de otra persona en su vida o en su mente, aunque seguramente será algo sin importancia. No bajes la guardia.");
+                } 
+            else {
+                        System.out.println("¡Enhorabuena! tu pareja parece ser totalmente fiel.");
+                  }
+            
+            }
+      
+      
+    
+    
+    
+    
+    
+    
+    
+      
+    
+    
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
