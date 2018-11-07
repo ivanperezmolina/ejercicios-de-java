f2ee023b8a3313d06bc0495c3dda1e040cd6e613
ivanperezmolina <ivan.perez.8500@gmail.com>
2018-11-07 08:50:28 +0100

    Corrigiendo ejercicios

--- Tema07/Ejercicio05.java
+++ Tema07/Ejercicio05.java
@@ -7,47 +7,51 @@
 public class Ejercicio05 {
   public static void main(String[] args) {
 	  
-	//Creo el array
-	
-	int[] miarray = new int[10];
-	
-	for(int i = 0; i<=9;i++){
-		String linea;
-		System.out.println("Introduce el numero ");
-		linea = System.console().readLine();		
-		miarray[i]= Integer.parseInt( linea ); 		
+		//Creo el array
 		
-	}
-	System.out.println("-----------------------");
-	
-	for(int j = 0; j<=9;j++){
-		System.out.println("En el hueco "+j+" tengo el numero "+miarray[j]);
-	}
-    /////////////////
-    
-    //Busco el máximo y el minimo
-    int max = 0;
-    int min = 0;
-    
-    for()
-    
- 
-
-    
-    /////////////////
-    
-    
-    		for(i = 0; i < myList.length; i++)
-		{
-			if(min>myList[i])
-			{
-				min=myList[i];
+		int[] miArray = new int[10];
+		
+		//Lee los datos
+		
+		for(int i = 0; i<=9;i++){
+			System.out.println("Introduce el numero: ");
+			String linea = System.console().readLine();		
+			miArray[i]= Integer.parseInt( linea ); 		
+			
+		}
+		System.out.println("-----------------------");
+		
+		//Calcular el max y min 
+		int maximo = miArray[0];
+		int minimo = miArray[0];
+		
+		for(int elemento : miArray){ //Otra forma para recorrer un array
+			if(elemento < minimo){
+				minimo = elemento;
 			}
-			if(max<myList[i])
-			{
-				max=myList[i];
+			
+			if(elemento > maximo){
+				maximo = elemento;
 			}
 		}
+		
+
+				
+		//Muestro los resultados		
+		for(int i = 0; i<=9;i++){
+			System.out.print(miArray[i]);
+			
+			if (miArray[i]==minimo){
+				System.out.print(" minimo ");
+			}
+			
+			if (miArray[i]==maximo){
+				System.out.print(" maximo ");
+			}
+			System.out.println("");			
+		}
+		
+
 
   }
 }
