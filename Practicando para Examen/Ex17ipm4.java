83bb47d9509ed4c4b63b92b4f13b99fd98096420
ivanperezmolina <ivan.perez.8500@gmail.com>
2018-12-05 11:14:12 +0100

    Practicas para el examen del martes sendero

--- Practicando para Examen/Ex17ipm4.java
+++ Practicando para Examen/Ex17ipm4.java
@@ -22,44 +22,64 @@ public class Ex17ipm4 {
 
   public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
-
+    //Pido la longitud del sendero
     System.out.print("Introduzca longitud del sendero en metros: ");
     int longitud = Integer.parseInt(s.nextLine());
     
+    //Le dejare un margen por delante para que al desplazar a la izquierda no
+    //se salga de la consola
     int margen = 14;
     
-    for (int i = 0; i < longitud; i++) {
-      margen += aleatorio(-1, 1);
-      System.out.print(repite(" ", margen));
+    for (int i = 0; i < longitud; i++) {//Hace esto segun la long. del sendero
+      margen += aleatorio(-1, 1);//Genero un aleatorio entre -1 y uno para decidir si se mueve a la izquierda, centro o derecha
+      System.out.print(repite(" ", margen));//Le meto a la funcion el caracter y cuantos espacios quiero que pinte
       
-      if (aleatorio(0, 1) == 0) {
-        System.out.println("#    #");
-      } else { // Hay obstáculo
-        int posicionObstaculo = aleatorio(1, 4);
-        System.out.print("#");
-        for (int j = 1; j <= 4; j++) {
-          if (j == posicionObstaculo) {
-            if (aleatorio(0, 1) == 0) {
-              System.out.print("*");
+      if (aleatorio(0, 1) == 0) {//Genero un numero para saber si en el camino me encontrare un obtaculo o no. 
+        System.out.println("#    #");//Si el if es cero; pinto el camino sin obtaculos
+      } else { // Si no es 0; significa que hay obtaculos
+        
+        int posicionObstaculo = aleatorio(1, 4);//Genero un numero entre el 1 y el 4 para saber en que posicion esta el obtaculo
+        System.out.print("#");//Abro el camino; este es el borde inicial
+        for (int j = 1; j <= 4; j++) {//Controlo en que hueco lo voy a poner
+          if (j == posicionObstaculo) {//Si coinciden el aleatorio generado con el valor de la j pinto. 
+            if (aleatorio(0, 1) == 0) {//Aqui decido si es un arbusto o una piedra
+              System.out.print("*");//Arbusto
             } else {
-              System.out.print("O");
+              System.out.print("O");//Piedra
             }
             
           } else {
-            System.out.print(" ");
+            System.out.print(" ");//Si no hay obtaculo, pinto un espacio
           }
         }
-        System.out.println("#");
+        System.out.println("#");//Cierro el camino, este es el borde final
       }
       
     }
   }
 
   
-
+  /**
+   * aleatorio-->Genera un numero aleatorio entre un valor minimo 
+   * introducido(inicio) y un valor maximo introducido(fin)
+   *
+   * @param inicio
+   * @param fin
+   * @return Un numero aleatorio entre un min y un max
+   */
   public static int aleatorio(int inicio, int fin) {
     return (int) (Math.random() * (fin - inicio + 1)) + inicio;
   }
+  
+  /////////////////////////////////////////////////////////////////////////////
+  /**
+   * repite-->Le paso un caracter(String) y un numero de veces para que me lo
+   * repita 
+   *
+   * @param cadena
+   * @param repeticiones
+   * @return Una cadena repetida "n" veces
+   */
 
   public static String repite(String cadena, int repeticiones) {
     String resultado = "";
